298ec7ab726a9443ad1b7dceeed053d5af0d84ee
julius <147092656+juliuspng@users.noreply.github.com>
2024-03-26 16:52:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -17,10 +17,23 @@ function cadastrarUsuarios() {
     let email = prompt('Digite um email para seu cadastro: ') //Email do usuário que logo será validado
     let senha = prompt('Digite uma senha para seu cadastro: ') //Senha do usuário
     let cargo = prompt('Digite o número respectivo ao seu cargo:\n1 - Cliente\n2 - Zelador\n3 - Atendente\n4 - Estoquista\n5 - Balconista\n6 - Gerente') //Cargo do usuário que logo será validado
-
     //Sistema de validação
 
-    function 
+    function validarCPF(x){ //Dizer se o CPF é um CPF válido ou não
+     if (x.length > 11 || x.length < 11){ //Um CPF possui 11 dígitos, algo diferente disso não seria um CPF
+        console.log('CPF inválido, cadastre novamente!') //Mensagem exibida caso o CPF seja inválido
+        cadastrarUsuarios() //Repete toda a operação caso o CPF seja inválido
+     }
+    }
+
+    function validarEmail(y){ //Dizer se o email é um email válido ou não
+    if (y.includes('@') == false){ //Caso não contenha "@", é um email inválido
+        cadastrarUsuarios() //Repete toda a operação caso o email seja inválido
+    }
+    if (y.includes('.') == false){  //Caso não contenha ".", é um email inválido
+        cadastrarUsuarios() //Repete toda a operação caso o email seja inválido
+    }
+    }
 
     /* 
        Aqui vocês implementam o código referente a função 
@@ -59,14 +72,17 @@ function logarUsuario() {
 
 function adicionarProduto() {
     let produto = prompt("Digite o nome do Filme que você vai adicionar ao estoque");
-    estoque.push(produto);
-    
+    estoque.push(produto);  //o push vai servir para jogar na variavel estoque
+}
        //Aqui vocês implementam o código referente a função 
     
-}
+
 
 function validadeIndividual() {
     let nome = prompt("Digite o nome do Filme para consultar sua validade:");
+    let produto =  estoque.find(item => item.nome === nome)
+
+    
     
        //Aqui vocês implementam o código referente a função 
     
@@ -133,47 +149,47 @@ console.log('******+++*******');
 console.log('Sistema de controle de estoque');
 console.log('******+++*******');
 
-/* 01 - Processo de cadastro de 6 usuários */
-for (let i = 0; i < 6; i++) {
-    cadastrarUsuarios();
-}
+// /* 01 - Processo de cadastro de 6 usuários */
+// for (let i = 0; i < 6; i++) {
+//     cadastrarUsuarios();
+// }
 
-/* 02 - Listar todos usuários cadastrados, com todas informações */
-listarUsuarios();
+// /* 02 - Listar todos usuários cadastrados, com todas informações */
+// listarUsuarios();
 
-/* 03 - Remover 1 dos usuários, qualquer um deles e depois listar novamente eles
-        para ver se o usuário foi removido. */
-removerUsuario();
-listarUsuarios();
+// /* 03 - Remover 1 dos usuários, qualquer um deles e depois listar novamente eles
+//         para ver se o usuário foi removido. */
+// removerUsuario();
+// listarUsuarios();
 
-/* 04 - Alterar algum dado do usuário */
-alterarDadoUsuario();
+// /* 04 - Alterar algum dado do usuário */
+// alterarDadoUsuario();
 
-/* 05 - Fazer login com um dos usuários cadastrados */
-logarUsuario();
+// /* 05 - Fazer login com um dos usuários cadastrados */
+// logarUsuario();
 
-/* 06 - Cadastrar pelo menos 15 produtos */
-adicionarProduto();
+// /* 06 - Cadastrar pelo menos 15 produtos */
+// adicionarProduto();
 
-/* 07 - Consultar validade individualmente de um produto */
-validadeIndividual();
+// /* 07 - Consultar validade individualmente de um produto */
+// validadeIndividual();
 
-/* 08 - Consultar validade de todos os produtos em formato relatório */
-imprimirRelatorioValidades();
+// /* 08 - Consultar validade de todos os produtos em formato relatório */
+// imprimirRelatorioValidades();
 
-/* 09 - Remover pelo menos 5 produtos */
-removerProduto();
+// /* 09 - Remover pelo menos 5 produtos */
+// removerProduto();
 
-/* 10 - Atualizar a quantidade de pelo menos 5 produtos */
-atualizarQuantidade();
+// /* 10 - Atualizar a quantidade de pelo menos 5 produtos */
+// atualizarQuantidade();
 
-/* 11 - Cadastrar fornecedor, no minimo 3 */
-cadastrarFornecedor();
+// /* 11 - Cadastrar fornecedor, no minimo 3 */
+// cadastrarFornecedor();
 
-/* 12 - Listar fornecedores */
-listarFornecedores();
+// /* 12 - Listar fornecedores */
+// listarFornecedores();
 
-/* 13 - Remover 1 fornecedor e depois imprimir a lista completa */
-removerFornecedor();
-listarFornecedores();
+// /* 13 - Remover 1 fornecedor e depois imprimir a lista completa */
+// removerFornecedor();
+// listarFornecedores();
 
